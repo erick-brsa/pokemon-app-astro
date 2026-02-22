4a2291d39fb6a96d63ac8a975dce4b300062bcf3
Erick Briones <erickalan518@gmail.com>
2026-02-21 23:29:48 -0600

    Mostrando favoritos

---
+++ src/components/Counter.tsx
@@ -0,0 +1,27 @@
+import { createSignal, type Component, type JSX } from "solid-js";
+
+interface Props {
+    initValue: number;
+    children?: JSX.Element;
+}
+
+export const Counter: Component<Props> = (props) => {
+    const [counter, setCounter] = createSignal(props.initValue);
+
+    return (
+        <>
+            {/* <h1 class="text-4xl">Counter</h1> */}
+            {props.children}
+            {/* <h1 class="text-4xl">{props.children}</h1> */}
+            <h3 class="text-xl">Value: {counter()} </h3>
+            <div>
+                <button
+                    onClick={() => setCounter(prev => ++prev)}
+                    class="bg-blue-500 p-2 mr-2 rounded">+1</button>
+                <button
+                    onClick={() => setCounter(prev => --prev)}
+                    class="bg-blue-500 p-2 mr-2 rounded">-1</button>
+            </div>
+        </>
+    );
+}
